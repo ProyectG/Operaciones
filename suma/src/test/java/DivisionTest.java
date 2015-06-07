7aefaf772f0b61a9da36b24e533c6108709a294e
ProyectG <marco.saffie@gmail.com>
2015-06-07 19:19:45 -0300

    prueba 02

---
+++ suma/src/test/java/DivisionTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.proyectg.division.Division;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Marco
+ */
+public class DivisionTest {
+    
+    public DivisionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void DivisionPorZero() {
+        Division demo = new Division();
+        double posible = 0;
+        double resultado = demo.division(12, 0);
+        assertEquals(posible,resultado);
+        
+    }
+}
